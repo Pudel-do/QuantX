9e8f505d75ee68cdaa4765c04112f01e87f04da9
matthias pudel <matthiaspudel@gmail.com>
2024-07-31 07:37:07 +0200

    Function for reading json files

--- GetAndProcessData.py
+++ GetAndProcessData.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import os
+import json
 from core.yfinance_adapter import YahooFinance
+from misc.misc import read_json
 
 if __name__ == "__main__":
     print("break")
---
+++ misc/misc.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+def read_json(file_name):
+    """
+    :param file_name: json file name to read
+    :type file_name: string
+    :return: Loaded json file
+    :rtype: dictionary
+    """
+    with open (os.path.join(os.getcwd(), "config", file_name)) as file:
+        config = json.load(file)
+
+    print("break")
+
+    return config
