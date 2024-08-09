def11d834c2535f4e09ad0f4ad3286c8da9dbbd4
matthias pudel <matthiaspudel@gmail.com>
2024-08-09 22:20:39 +0200

    Implementing fundamental data extraction

--- GetAndProcessData.py
+++ GetAndProcessData.py
@@ -5,8 +5,9 @@ import json
 from datetime import datetime
 from core.finance_adapter import FinanceAdapter
 from misc.misc import read_json
+from itertools import chain
 
-def combine_quotes(ticker, start, quote_id):
+def concat_quotes(ticker_list, start, quote_id):
     end = datetime.now()
     quotes = pd.DataFrame(
         index=pd.date_range(
@@ -17,27 +18,42 @@ def combine_quotes(ticker, start, quote_id):
         )
     )
 
-    for tick in ticker:
-        ticker_quotes = FinanceAdapter().get_quotes(tick=tick, start=start)
+    for tick in ticker_list:
+        ticker_quotes = FinanceAdapter(tick).get_quotes(start=start)
         ticker_quote = ticker_quotes[quote_id]
         ticker_quote.name = tick
         quotes = quotes.join(ticker_quote)
     return quotes
-    
-def combine_fundamentals(ticker, start):
-    liste = []
-    for tick in ticker:
-        balance_sheet = FinanceAdapter().get_balance_sheet(tick=tick)
-        income_statement = FinanceAdapter().get_income_statement(tick=tick)
-        print("break")
 
-    return liste
+def calculate_returns(quotes):
+    rets = np.log(quotes / quotes.shift(1))
+    rets = rets.iloc[1:]
+    return rets
     
+def preprocess_fundamentals(ticker_list, start):
+    fundamental_dict = {}
+    for tick in ticker_list:
+        income_statement = FinanceAdapter(tick).get_fundamental(fd_kpi="income")
+        balance_sheet = FinanceAdapter(tick).get_fundamental(fd_kpi="balance_sheet")
+        cashflow = FinanceAdapter(tick).get_fundamental(fd_kpi="cashflow")
+        fundamentals = pd.concat(
+            objs=[
+                income_statement,
+                balance_sheet,
+                cashflow
+            ],
+            axis=1
+        )
+        fundamental_dict[tick] = fundamentals
+    return fundamental_dict
 
 if __name__ == "__main__":
-    ticker = read_json("inputs.json")["ticker"]
+    ticker_list = read_json("inputs.json")["ticker"]
     base_start = read_json("inputs.json")["base_start"]
-    #closing_quotes = combine_quotes(ticker=ticker, start=base_start, quote_id="Adj Close")
-    fundamental = combine_fundamentals(ticker, base_start)
+    closing_quotes = concat_quotes(ticker_list=ticker_list, 
+                                   start=base_start, 
+                                   quote_id="Adj Close")
+    returns = calculate_returns(closing_quotes)
+    fundamental_data = preprocess_fundamentals(ticker_list, base_start)
 
 
--- core/finance_adapter.py
+++ core/finance_adapter.py
@@ -5,7 +5,6 @@ import requests
 import warnings
 import logging
 from core import logging_config
-from financetoolkit import Toolkit
 from alpha_vantage.fundamentaldata import FundamentalData
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -16,9 +15,15 @@ class FinanceAdapter:
         self.tick = tick
         self.fx_config = read_json("exchange_rates.json")
         self.fd_config = read_json("constant.json")["fundamentals"]
-        pass
 
     def get_quotes(self, start):
+        """Function extracts all converted price data information and volume data for single ticker
+
+        :param start: Start date for retrieving stock data for single ticker
+        :type start: string
+        :return: Dataframe with all price and volume data information. 
+        :rtype: Dataframe
+        """
         end = datetime.now()
         quotes = yf.download(
             tickers=self.tick,
@@ -32,11 +37,12 @@ class FinanceAdapter:
         base_currency = self.fx_config["base_currency"]
         if not currency == base_currency:
             quotes = self._quote_converter(quotes, currency)
-
         return quotes
     
     def get_fundamental(self, fd_kpi):
-        fd = self._establish_fmb_connection(fd_kpi=fd_kpi)
+        fd_list = self._establish_fmb_connection(fd_kpi=fd_kpi)
+        fd_list_converted = self._fundamental_converter(fd_list=fd_list)
+        fd = pd.DataFrame(fd_list_converted).set_index("date")
         return fd
 
     def _establish_fmb_connection(self, fd_kpi):
@@ -46,21 +52,56 @@ class FinanceAdapter:
         url = f"https://financialmodelingprep.com/api/v3/{fd_kpi_value}/{self.tick}?period=annual&apikey={api_key}"
         response = requests.get(url)
         if response.status_code == 200:
-            fd_data = response.json()
-            return fd_data
+            fd_list = response.json()
+            return fd_list
         else:
             logging.error(f"Connection to FMP failed for ticker {self.tick}")
             return []
+        
+    def _fundamental_converter(self, fd_list):
+        fd_list_converted = []
+        for item in fd_list:
+            fx_ticker_mapping = self.fx_config["currency_ticker"]
+            currency = item["reportedCurrency"]
+            fd_start_date = get_business_day(item["date"])
+            fd_end_date = pd.to_datetime(fd_start_date)
+            fd_end_date = fd_end_date + pd.offsets.BDay(1)
+            fd_end_date = fd_end_date.strftime(format="%Y-%m-%d")
+            try:
+                fx_ticker = fx_ticker_mapping[currency]
+                try:
+                    fx_rate = yf.download(
+                        tickers=fx_ticker,
+                        start=fd_start_date,
+                        end=fd_end_date,
+                        progress=False,
+                        interval="1d",
+                        ignore_tz=True,
+                    )
+                    converter = float(fx_rate["Close"].values)
+                except KeyError:
+                    logging.error(f"Exchange rate download failed for {fx_ticker}")
+            except KeyError:
+                logging.warning(f"Ticker {currency} not in config file")
+                converter = 1
+            fd_dict = {
+                key: value * converter if isinstance(value, int) \
+                    and not currency == self.fx_config["base_currency"] \
+                    else value for key, value in item.items()
+                    }
+            fd_list_converted.append(fd_dict)
+        return fd_list_converted
+
 
     def _quote_converter(self, data, currency):
-        try:
-            exchange_rate_ticker = self.fx_config["currency_ticker"]
-            ticker = exchange_rate_ticker[currency]
+        fx_ticker_mapping = self.fx_config["currency_ticker"]
+        try:    
+            fx_ticker = fx_ticker_mapping[currency]
             try:
                 start = data.index[0]
                 end = data.index[-1]
-                exchange_rates = yf.download(
-                    tickers=ticker,
+                fx_rate = yf.download(
+                    tickers=fx_ticker,
                     start=start,
                     end=end,
                     progress=False,
@@ -69,11 +110,11 @@ class FinanceAdapter:
                 )["Close"]
                 
                 converter = pd.DataFrame(index=data.index)
-                converter = converter.join(exchange_rates)
+                converter = converter.join(fx_rate)
                 converter = converter.ffill()
                 converter = np.array(converter).flatten()
             except KeyError:
-                logging.error(f"Download failed for exchange rate symbol {ticker}")
+                logging.error(f"Exchange rate download failed for {fx_ticker}")
         except KeyError:
             logging.warning(f"Ticker {currency} not in config file")
             converter = np.ones(shape=(len(data), ))
@@ -81,8 +122,5 @@ class FinanceAdapter:
         gen = (col for col in data.columns if not col == "Volume")
         for col in gen:
             data[col] = data[col] * converter
-            
         return data
     
-    def _fundamental_converter(self):
-        pass
