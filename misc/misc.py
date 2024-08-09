17cdf93e35eff02465dc06b3715f4c67ada20aa2
matthias pudel <matthiaspudel@gmail.com>
2024-08-09 22:21:47 +0200

    Add function for calculating the relevant business day

--- misc/misc.py
+++ misc/misc.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from datetime import datetime
 import json
 import os
 
@@ -12,3 +14,21 @@ def read_json(file_name):
         config = json.load(file)
 
     return config
+
+def get_business_day(date):
+    """
+    :param date: Base date for calculating the previous business day
+    :type date: string or date object
+    :return: Previous business day for given date. 
+    If param date is still a business day, the function returns param date
+    :rtype: string
+    """
+    if not isinstance(date, datetime):
+        date = pd.to_datetime(date)
+    is_bday = pd.bdate_range(start=date, end=date).shape[0] > 0
+    if not is_bday:
+        bday = date - pd.offsets.BDay(1)
+        bday = bday.strftime(format="%Y-%m-%d")
+    else:
+        bday = date.strftime(format="%Y-%m-%d")
+    return bday
