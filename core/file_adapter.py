0bab6996bc3fd227bd736da2b7c8f498d1e23b61
matthias pudel <matthiaspudel@gmail.com>
2024-08-14 07:45:04 +0200

    Add file adapter for data modelling

--- core/file_adapter.py
+++ core/file_adapter.py
@@ -2,8 +2,34 @@ import pandas as pd
 import os
 from misc.misc import read_json
 
-class DataConnector:
+class FileAdapter:
     def __init__(self) -> None:
         self.config = read_json("constant.json")["datamodel"]
 
-    
+    def write_stock_returns(self, data):
+        dir = os.path.join(os.getcwd(), self.config["data_path"])
+        file_name = self.config["returns_file"]
+
+        pass
+
+    def _write_csv(self, data, dir, file_name):
+        self._dir_check(dir=dir)
+        path = os.path.join(dir, file_name)
+        data.to_csv(data,
+                    sep=",",
+                    decimal=".",
+                    )
+        pass
+
+    def _dir_check(self, dir):
+        """Function checks if given directory exists.
+        If directory not exists, the function creates directory
+
+        :param dir: Directory to check for
+        :type dir: String
+        :return: None
+        :rtype: None
+        """
+        if not os.path.exists(dir):
+            os.mkdir(dir) 
+        return None
