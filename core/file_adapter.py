ae5f1e9e1e6b12670cf6cb02ba3a912e4397cf3b
matthias pudel <matthiaspudel@gmail.com>
2024-08-18 16:01:04 +0200

    Code docs and function for writing return file to working directory path

--- core/file_adapter.py
+++ core/file_adapter.py
@@ -6,20 +6,45 @@ class FileAdapter:
     def __init__(self) -> None:
         self.config = read_json("constant.json")["datamodel"]
 
-    def write_stock_returns(self, data):
-        dir = os.path.join(os.getcwd(), self.config["data_path"])
-        file_name = self.config["returns_file"]
+    def write_stock_rets(self, rets):
+        """Function writes stock return data
+        to data directory as csv file
 
-        pass
+        :param rets: Stock returns
+        :type data: Dataframe
+        :return: None
+        :rtype: None
+        """
+        dir = os.path.join(os.getcwd(), self.config["data_dir"])
+        file_name = self.config["returns_file"]
+        self._write_csv(
+            data=rets,
+            dir=dir,
+            file_name=file_name
+        )
+        return None
 
     def _write_csv(self, data, dir, file_name):
+        """Function writes data to working
+        directory as csv file
+
+        :param data: Data to write to working directory
+        :type data: Dataframe
+        :param dir: Directory where data should be written to
+        :type dir: String
+        :param file_name: File name for saving data
+        :type file_name: String
+        :return: None
+        :rtype: None
+        """
         self._dir_check(dir=dir)
         path = os.path.join(dir, file_name)
-        data.to_csv(data,
+        data.to_csv(path,
                     sep=",",
                     decimal=".",
+                    index=True
                     )
-        pass
+        return None
 
     def _dir_check(self, dir):
         """Function checks if given directory exists.
@@ -32,4 +57,6 @@ class FileAdapter:
         """
         if not os.path.exists(dir):
             os.mkdir(dir) 
+        else:
+            pass
         return None
