41a638cffa39f5aba2964d83003b924f8c569f66
matthias pudel <matthiaspudel@gmail.com>
2024-08-23 19:47:05 +0200

    Implement heatmap with time slider

--- core/dashboard_adapter.py
+++ core/dashboard_adapter.py
@@ -1,6 +1,7 @@
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
+import plotly.express as px
 import time
 import threading
 import webbrowser
@@ -17,16 +18,32 @@ class AnalysisDashboard:
         self._register_callbacks()
 
     def _setup_layout(self):
+        ma_tickers = self.ma_data["Ticker"].unique()
+        return_tickers = self.returns.columns
+        tickers = list(set(ma_tickers) & set(return_tickers))
+        date_range = self.returns.index
         self.app.layout = html.Div(
             [
                 dcc.Dropdown(
                     id="Dropdown Ticker",
                     options=[{'label': ticker, 'value': ticker} \
-                             for ticker in self.tickers],
-                    value=self.tickers[0]
+                             for ticker in tickers],
+                    value=tickers[0]
                 ),
                 dcc.Graph(id="LineChart"),
-                dcc.Graph(id="Histogram")
+                dcc.Graph(id="Histogram"),
+                html.Label("Adjust Time Period for correlation matrix"),
+                dcc.RangeSlider(
+                    id="Date Range Slider",
+                    min=0,
+                    max=len(date_range) - 1,
+                    value=[0, len(date_range) - 1],
+                    marks={i: date.strftime('%Y-%m') \
+                           for i, date in enumerate(date_range) \
+                            if date.month == 1},
+                    tooltip={"placement": "bottom", "always_visible": True}
+                ),
+                dcc.Graph(id="Heatmap")
             ]
         )
 
@@ -36,7 +53,7 @@ class AnalysisDashboard:
              Output("Histogram", "figure")],
             [Input("Dropdown Ticker", "value")]
         )
-        def update_charts(selected_ticker):
+        def update_dropdwon_charts(selected_ticker):
             filter_mask_line = self.ma_data["Ticker"] == selected_ticker
             filtered_line = self.ma_data[filter_mask_line]
             filtered_hist = self.returns[selected_ticker]
@@ -81,6 +98,28 @@ class AnalysisDashboard:
                 'layout': {'title': f'Histogram for {selected_ticker} returns'}
             }
             return line_chart_fig, hist_fig
+        
+        @self.app.callback(
+            Output('Heatmap', 'figure'),
+            [Input('Date Range Slider', 'value')]
+        )    
+        def update_slider_charts(slider_value):
+            # Determine the end date for the correlation period based on the slider value
+            full_date_range = self.returns.index
+            start_date = full_date_range[slider_value[0]]
+            end_date = full_date_range[slider_value[1]]
+
+            # Filter the returns DataFrame up to the selected date
+            filtered_returns_df = self.returns[(self.returns.index >= start_date) & (self.returns.index <= end_date)]
+
+            # Calculate the correlation matrix
+            correlation_matrix = filtered_returns_df.corr()
+
+            # Create the heatmap figure
+            heatmap_figure = px.imshow(correlation_matrix, text_auto=True, aspect="auto", color_continuous_scale='RdBu_r')
+            heatmap_figure.update_layout(title=f"Return Correlation Heatmap ({start_date.strftime('%Y-%m-%d')} to {end_date.strftime('%Y-%m-%d')})")
+
+            return heatmap_figure
         
     def run(self, debug=True):
 
