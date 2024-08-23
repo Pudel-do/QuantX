d1c7c6c828c92ca35515db251dc00be8fef3d43a
matthias pudel <matthiaspudel@gmail.com>
2024-08-23 18:37:14 +0200

    Change so simple moving average

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -9,18 +9,19 @@ def get_moving_average(quotes, ma_days):
     ma_dict = {}
     days_short = ma_days[0]
     days_long = ma_days[1]
+    quotes = quotes.dropna()
     for name, values in quotes.items():
         values.name = "Quote"
-        ma_short = values.ewm(
-            halflife=0.5,
-            min_periods=days_short
+        ma_short = values.rolling(
+            window=days_short
         ).mean()
-        ma_short.name = f"MA_{days_short}"
-        ma_long = values.ewm(
-            halflife=0.5,
-            min_periods=days_long
+        ma_short.name = f"MovingAverage{days_short}"
+
+        ma_long = values.rolling(
+            window=days_long
         ).mean()
-        ma_long.name = f"MA_{days_long}"
+        ma_long.name = f"MovingAverage{days_long}"
+
         ma_df = pd.concat(
             objs=[values, ma_short, ma_long],
             axis=1,
@@ -38,21 +39,24 @@ def concat_ma_dict(ma_dict):
         objs=ma_dict.values(),
         axis=0,
         ignore_index=False
-    )
+        )
     return ma_concat
         
 
 
 if __name__ == "__main__":
     parameter = read_json("parameter.json")["analysis"]
+    ticker_list = read_json("inputs.json")["ticker"]
     ma_days = parameter["ma_days"]
     closing_quotes = FileAdapter().load_closing_quotes()
+    returns = FileAdapter().load_returns()
     moving_average_dict = get_moving_average(
         quotes=closing_quotes, 
         ma_days=ma_days
         )
     concat_moving_averages = concat_ma_dict(moving_average_dict)
-    app = AnalysisDashboard(quotes=concat_moving_averages)
+    app = AnalysisDashboard(tickers=ticker_list,
+                            ma_data=concat_moving_averages,
+                            returns=returns)
     app.run()
-    print("Finished")
 
