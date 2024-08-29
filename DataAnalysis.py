55c1a152989b8cf095245a96e3d0fd67a8cece61
matthias pudel <matthiaspudel@gmail.com>
2024-08-29 07:50:02 +0200

    Function to transform dict to dataframe for fundamental data

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -6,6 +6,19 @@ from core.dashboard_adapter import AnalysisDashboard
 from misc.misc import *
 
 def get_moving_average(quotes, ma_days):
+    """Function calculates simple moving averages for given
+    amount of short period and long period days in inputs.json file. 
+    All missing values are removed for calculation for the underlying 
+    quote data
+
+    :param quotes: Quotes data for given stocks
+    :type quotes: Dataframe
+    :param ma_days: Days for calculating moving averages
+    :type ma_days: List
+    :return: Dictionary with ticker symbols as key and
+    quote and moving averages as value
+    :rtype: Dictionary
+    """
     ma_dict = {}
     days_short = ma_days[0]
     days_long = ma_days[1]
@@ -31,36 +44,54 @@ def get_moving_average(quotes, ma_days):
         ma_dict[name] = ma_df
     return ma_dict
 
-def concat_ma_dict(ma_dict):
-    for key, value in ma_dict.items():
-        value["Ticker"] = key
+def concat_dict_to_df(dict):
+    """Function concats a python dictionary
+    to a pandas dataframe
 
-    ma_concat = pd.concat(
-        objs=ma_dict.values(),
+    :param ma_dict: Dictionary with ticker as key
+    :type ma_dict: Dictionary
+    :return: Dataframe with dictionary values as column
+    :rtype: Dataframe
+    """
+    constant_cols = read_json("constant.json")["columns"]
+    tick_col = constant_cols["ticker"]
+    for key, value in dict.items():
+        value[tick_col] = key
+
+    df = pd.concat(
+        objs=dict.values(),
         axis=0,
         ignore_index=False
         )
-    return ma_concat
-        
-
+    return df
 
 if __name__ == "__main__":
     parameter = read_json("parameter.json")["analysis"]
     ticker_list = read_json("inputs.json")["ticker"]
     fundamental_list = read_json("constant.json")["fundamentals"]["measures"]
     ma_days = parameter["ma_days"]
-    closing_quotes = FileAdapter().load_closing_quotes()
     returns = FileAdapter().load_returns()
-    fundamentals = FileAdapter().load_fundamentals()
+    closing_quotes = FileAdapter().load_closing_quotes()
+    fundamentals_dict = FileAdapter().load_fundamentals()
+    closing_quotes = harmonize_tickers(object=closing_quotes)
+    fundamentals_dict = harmonize_tickers(object=fundamentals_dict)
     moving_average_dict = get_moving_average(
         quotes=closing_quotes, 
         ma_days=ma_days
         )
-    app = AnalysisDashboard(tickers=ticker_list,
-                            ma_data=moving_average_dict,
-                            returns=returns,
-                            fundamentals=fundamentals,
-                            fundamental_list=fundamental_list)
+    moving_average_df = concat_dict_to_df(
+        dict=moving_average_dict
+        )
+    fundamentals_df = concat_dict_to_df(
+        dict=fundamentals_dict
+    )
+    app = AnalysisDashboard(
+        tickers=ticker_list,
+        ma_data=moving_average_df,
+        returns=returns,
+        fundamentals=fundamentals_df,
+        fundamental_list=fundamental_list
+        )
     app.run()
     print("Finished")
 
