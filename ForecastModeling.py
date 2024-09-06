64226d74ac773a895824e924c0073c3bf93bb49d
matthias pudel <matthiaspudel@gmail.com>
2024-09-06 14:15:57 +0200

    Add forecast models and scripts

---
+++ ForecastModeling.py
@@ -0,0 +1,16 @@
+def merge_features():
+    """Function merges feature data for model building
+    """
+    pass
+
+def data_cleaning():
+    """Function removes missing values and filters for model time period
+    """
+    pass
+
+def feature_engineering():
+    """Function calculates features for model building
+    Output should display processed data for model building
+    """
+    pass
+
