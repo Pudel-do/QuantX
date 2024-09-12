a52de623dec6d60f4cdbb6794053a5f9e4146455
matthias pudel <matthiaspudel@gmail.com>
2024-09-12 21:27:24 +0200

    Add function for evaluating lstm model

--- core/models.py
+++ core/models.py
@@ -2,15 +2,19 @@ import numpy as np
 import pandas as pd
 import keras
 import tensorflow as tf
+import io
+import matplotlib.pyplot as plt
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from keras import layers
 from core.base_model import BaseModel
 from misc.misc import *
 
 class lstm(BaseModel):
-    def __init__(self):
-        super().__init__(model_name="LSTM")
+    def __init__(self, ticker):
+        super().__init__(model_name="LSTM", ticker=ticker)
+        self._create_model_id()
 
     def preprocess_data(self):
         """Function preprocesses raw model data for
@@ -27,6 +31,7 @@ class lstm(BaseModel):
         :rtype: None
         """
         scaled_data = self._data_scaling(data=self.data)
+        self.scaled_data = scaled_data
         seq_length = self.params["sequence_length"]
         exog, endog = [], []
         for i in range(seq_length, len(scaled_data)):
@@ -54,23 +59,24 @@ class lstm(BaseModel):
         sequences = self.x_train.shape[1]
         n_features = self.x_train.shape[2]
         input_shape = (sequences, n_features)
-        model.add(
-            layers.LSTM(
-                units=50, 
-                return_sequences=True, 
-                input_shape=input_shape
-            )
-        )
-        model.add(layers.LSTM(units=50))
+        model.add(layers.Input(shape=input_shape))
+        model.add(layers.LSTM(units=100, return_sequences=True))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.LSTM(units=50, return_sequences=False))
+        model.add(layers.Dropout(0.2))
         model.add(layers.Dense(units=1))
         model.compile(
             optimizer="adam", 
-            loss='mean_squared_error'
+            loss='mean_squared_error',
+            metrics=[
+                "root_mean_squared_error",
+                "mean_absolute_error"
+            ]
         )
         self.model = model
         return None
     
-    def train_model(self):
+    def train(self):
         """Function trains LSTM model and saves callbacks
         in model logs. Trained model is inherited to 
         model class
@@ -78,18 +84,66 @@ class lstm(BaseModel):
         :return: None
         :rtype: None
         """
-        run_logdir = self._get_log_path()
-        tb_callbacks = keras.callbacks.TensorBoard(run_logdir)
+        log_dir = self._get_log_path(log_key="training_logs")
+        tb_callbacks = keras.callbacks.TensorBoard(log_dir)
         self.model.fit(
             self.x_train,
             self.y_train,
             epochs=self.params["epochs"],
+            batch_size=32,
             validation_data=(self.x_test, self.y_test),
-            callbacks=[tb_callbacks]
+            callbacks=[tb_callbacks],
         )
         return None
+    
+    def evaluate(self):
+        y_pred = self.model.predict(x=self.x_test)
+        y_pred = y_pred.reshape(-1,1)
+        target = self.y_test.reshape(-1,1)
+        y_pred = self.target_scaler.inverse_transform(y_pred)
+        target = self.target_scaler.inverse_transform(target)
+        mse = mean_squared_error(y_true=target, y_pred=y_pred)
+        rmse = np.sqrt(mse)
+        rmse = np.round(rmse, 3)
+        log_dir = self._get_log_path(log_key="evaluation_logs")
+        file_writer = tf.summary.create_file_writer(log_dir)
+        figure = self._create_plot(
+            target=target, 
+            y_pred=y_pred, 
+            rmse=rmse
+        )
+        with file_writer.as_default():
+            tf.summary.image(
+                "Real vs Predicted", 
+                self._plot_to_image(figure), 
+                step=0
+            )
+        return None
+    
+    def predict(self):
+        pass
+    
+    def _create_plot(self, target, y_pred, rmse):
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(target, label="Real Values", color='blue')
+        ax.plot(y_pred, label="Predicted Values", color='red')
+        ax.legend()
+        ax.set_title(f"In-Sample prediction for {self.ticker} with RMSE of {rmse}")
+        ax.set_xlabel("Sample Index")
+        ax.set_ylabel("Value")
+        return fig
+    
+    def _plot_to_image(self, figure):
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        plt.close(figure)
+        buf.seek(0)
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        image = tf.expand_dims(image, 0)  # Add batch dimension
+        return image
 
-    def _get_log_path(self):
+
+    def _get_log_path(self, log_key):
         """Function builds path for saving model logs.
         Underlying directory elements are defined in 
         constant.json
@@ -97,10 +151,15 @@ class lstm(BaseModel):
         :return: Path for saving model logs
         :rtype: String
         """
-        datamodel = read_json("constant.json")["datamodel"]
-        models_dir = datamodel["models_dir"]
-        log_dir = datamodel["model_logs"]
-        file_name = datetime.now().strftime("%Y%m%d-%H%M%S")
-        path = os.path.join(os.getcwd(), models_dir, log_dir, file_name)
+        data_model = read_json("constant.json")["datamodel"]
+        models_dir = data_model["models_dir"]
+        log_dir = data_model[log_key]
+        path = os.path.join(
+            os.getcwd(), 
+            models_dir,
+            self.ticker,
+            log_dir, 
+            self.model_id
+        )
         return path
 
