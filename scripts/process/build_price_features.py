b0361fe15f50770232f48c6a69be6d59eccd3abc
matthias pudel <matthiaspudel@gmail.com>
2025-12-30 11:45:50 +0100

    Draft for feature price calculation

---
+++ scripts/process/build_price_features.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+from core.finance_adapter import FinanceAdapter
+from misc.utils import read_json, get_last_business_day
+from misc.schema_utils import *
+
+
+def read_prices(ticker_id):
+
+    sql = """
+        SELECT ticker_id, date, adj_close, volume
+        FROM raw_prices
+        WHERE ticker_id = :ticker_id
+        ORDER BY date
+        """
+    
+    df = read_sql_as_df(
+        sql=sql,
+        params={
+            "ticker_id": ticker_id
+        }
+    )
+    
+    return df
+
+def calculate_features():
+    pass
+
+def run():
+    tickers = read_sql_as_df(
+        sql= """
+            SELECT ticker_id, ticker
+            FROM tickers
+            """,
+        params=None
+    )
+    for _, row in tickers.iterrows():
+        prices = read_prices(row["ticker_id"])
+
+if __name__ == "__main__":
+    run()
