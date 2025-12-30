b64ca65c69425d7d5a7f426cc076b797df279a5a
matthias pudel <matthiaspudel@gmail.com>
2025-12-30 08:03:52 +0100

    Change attributes to lowercase

--- sql/schema/01_tickers.sql
+++ sql/schema/01_tickers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tickers (
-    TICKER_ID INTEGER PRIMARY KEY,
-    TICKER TEXT UNIQUE NOT NULL,
-    NAME_LONG TEXT,
-    NAME_SHORT TEXT,
-    CURRENCY TEXT
+    ticker_id INTEGER PRIMARY KEY,
+    ticker TEXT UNIQUE NOT NULL,
+    long_name TEXT,
+    short_name TEXT,
+    currency TEXT
 );
