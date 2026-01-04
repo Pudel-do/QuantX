5e37cf3918a0a765f349aac413903f6e43443a2d
matthias pudel <matthiaspudel@gmail.com>
2026-01-04 19:00:27 +0100

    Quick fix

--- sql/schema/01_assets.sql
+++ sql/schema/01_assets.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS assets (
-    asset_id INTEGER PRIMARY KEY,
+    asset_id INTEGER NOT NULL PRIMARY KEY,
     ticker TEXT UNIQUE NOT NULL,
     long_name TEXT,
     short_name TEXT,
